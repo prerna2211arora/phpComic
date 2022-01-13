a59289e4a2995625c4c27ef1b317b02c3480eef2
prerna2211arora <prernaarora221@gmail.com>
2022-01-13 22:50:59 +0530

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -1,10 +1,16 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "root";
-  $dbname = "rtcamp_db";
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
   if(!$conn){
     header("location: /error.php?error=Database Error");
   }
